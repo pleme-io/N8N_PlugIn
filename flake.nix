81daf303f7842329b8f02ea8e881a2ab0eebd96e
drzzln <drzzln@protonmail.com>
2026-03-15 19:56:04 -0400

    add nix flake with substrate integration

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "n8n community node for Akeyless Secrets Management integration with API Key authentication";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      packages.default = pkgs.buildNpmPackage {
+        pname = "n8n-nodes-akeyless";
+        version = "0.0.0-dev";
+        src = self;
+        sourceRoot = "source/n8n-nodes-akeyless";
+        npmDepsHash = "sha256-i7qJKvYzYdVm/ZpWW9iM9Ica65NV1PegEDDkmoSM9Mc="; # TODO: set correct hash
+        dontNpmBuild = false;
+        npmBuildScript = "build";
+        meta = {
+          description = "n8n community node for Akeyless Secrets Management integration with API Key authentication";
+          homepage = "https://github.com/pleme-io/N8N_PlugIn";
+          license = pkgs.lib.licenses.mit;
+        };
+      };
+
+      devShells.default = pkgs.mkShellNoCC {
+        packages = with pkgs; [ nodejs_22 ];
+      };
+    });
+}
